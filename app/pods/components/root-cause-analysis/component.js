36520b68e5d57212973ac9495fb802c50405c203
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-22 10:53:00 -0400

    Refactor for randomly generated data

--- app/pods/components/root-cause-analysis/component.js
+++ app/pods/components/root-cause-analysis/component.js
@@ -8,27 +8,45 @@ export default Component.extend({
     computeToast: false,
     editEdge: false,
     recompute: false,
-    // /* 
-    //  * Description: Root signature is the 'cause signature' we are going to be comparing all other paths to
-    //  * Complexity: constant time
-    //  * Purpose: Not sure if this is 100% necessary as of now
-    // */
-    // rootSignature: computed('startNode', 'endNode', function() {
-    //      const startNode = this.get('startNode')
-    //      const endNode = this.get('endNode')
+    pathDetails: false,
+    numNodes: 10,
+    branching: 2,
 
-    //      const rootEdge = this.get('objectMap').get(startNode + '-' + endNode)
+    nodes: computed( 'model', function() {
+        let nodes = []
+        for( let i = this.get('numNodes'); i > 0; i--){
+            nodes.push({"id": i.toString()})
+        }
+        return nodes
+    }),
 
-    //     for(const )
-    //     }),
+    edges: computed( 'nodes', function() {
+        const branching = this.get('branching')
+        const numNodes = this.get('numNodes')
 
+        let edges = []
+        let usedPairs = new Set()
+        for(let i = 0; i < branching*numNodes; i++){
+            let source = i % numNodes
+            let target = (Math.floor(Math.random() * numNodes) + 1 )
+            while(usedPairs.has(source + '-' + target) || source == target){
+                target = (Math.floor(Math.random() * numNodes) + 1 )
+            }
+            usedPairs.add(source + '-' + target)
+            
+            edges.push({'id': source + '-' + target ,'label': source + '-' + target, source, target, 'data': 0})
+        }
+        this.send('updateRoot', edges[0])
+        return edges
+    }),
+    
     /* 
      * Description: Map of edge id's and data to be querried by computedPaths
      * Complexity: linear time to construct
      * Purpose: This is meant to allow contant time access to this information
     */
-    objectMap: computed( 'model.@each', function() {
-        const edges = this.get('model')
+    objectMap: computed( 'edges', function() {
+        const edges = this.get('edges')
         let objectMap = new Map()
         for(const edge of edges){
             objectMap.set(edge.id, edge)
@@ -41,12 +59,12 @@ export default Component.extend({
      * Complexity: Exponential time complexity (doesnt scale)
      * Purpose: This is meant to allow comparison from the heuristic based solution to caclulate successfull root cause analysis
     */
-    allPaths: computed( 'model', 'root', 'recompute', function() {
+    allPaths: computed( 'root', function() {
         const startTime = moment.now()
         let iterations = 0
         
-        const edges = this.get('model')
-        const root = this.get('root') || this.get('objectMap').get('0-1')
+        const edges = this.get('edges')
+        const root = this.get('root')
         const startNode = root.source
         const endNode = root.target
 
@@ -79,9 +97,8 @@ export default Component.extend({
                 curr = null
             }
         }
-
         this.setStats({'time': (moment.now() - startTime) /1000, 'iterations': iterations})
-        return paths.sort(function(a, b) { return a.length - b.length || a.localeCompare(b)})
+        return paths.sort(function(a, b) { return a.length - b.length})
     }),
 
     /* 
@@ -92,22 +109,44 @@ export default Component.extend({
     computedPaths: computed( 'allPaths', function() {
         const allPaths = this.get('allPaths')
         const objectMap = this.get('objectMap')
+        const root = this.get('root')
         let computedPaths = []
 
         for(const path of allPaths){
             let computedPath = []
+            let firstPath = objectMap.get(path[0] + '-' + path[1]) || objectMap.get(path[1] + '-' + path[0])
+            for(let i = 0; i< firstPath.data.length; i++){
+                for(let j in firstPath.data[i].metrics){
+                    for(let k in firstPath.data[i].metrics[j]){
+                        if(firstPath.data[i].metrics[j][k] != 0 && root) {
+                            //firstPath.data[i].metrics[j][k] = firstPath.data[i].metrics[j][k]/root.data[i].metrics[j][k]  || 0
+                        }
+                    }
+                }
+            }
             computedPath.push({
                 'label': path[0], 
-                'data': objectMap.get(path[0] + '-' + path[1]) || objectMap.get(path[1] + '-' + path[0])
+                'data': firstPath
             })
             for(let i = 1; i <path.length; i++){
+                let nextPath =  objectMap.get(path[i-1] + '-' + path[i]) || objectMap.get(path[i] + '-' + path[i-1])
+                for(let i = 0; i< nextPath.data.length; i++){
+                    for(let j in nextPath.data[i].metrics){
+                        for(let k in nextPath.data[i].metrics[j]){
+                            if(nextPath.data[i].metrics[j][k] != 0 && root && root.data[i].metrics[j][k] != 0) {
+                                //nextPath.data[i].metrics[j][k] = nextPath.data[i].metrics[j][k]/root.data[i].metrics[j][k]
+                            }
+                        }
+                    }
+                }
                 computedPath.push({
                     'label': path[i], 
-                    'data': objectMap.get(path[i-1] + '-' + path[i]) || objectMap.get(path[i] + '-' + path[i-1])
+                    'data': nextPath
                 })
             }
+            
             computedPaths.push(computedPath)
-
+            
         }
         return computedPaths
     }),
@@ -133,11 +172,11 @@ export default Component.extend({
             //This is just to force the CP to recompute
             item.type = item.type == 'arrow' ? 'line' : 'arrow'
             this.toggleProperty('editEdge')
-            let model = this.get('model')
+            let edges = this.get('edges')
             if(action == 'save'){
-                for(let i = 0; i<model.length; i++){
-                    if(model[i].label === item.label){
-                        this.set('model'+ [i],item)
+                for(let i = 0; i<edges.length; i++){
+                    if(edges[i].label === item.label){
+                        this.set('edges'+ [i],item)
                     }
                 }
                 this.toggleProperty('recompute')
@@ -145,8 +184,18 @@ export default Component.extend({
             
             
         },
+        toggleRecompute() {
+            this.toggleProperty('recompute')
+        },
+        togglePathDetails(selectedItem) {
+            this.set('selectedItem', selectedItem)
+            this.toggleProperty('pathDetails')
+        },
         toggleToast() {
             this.toggleProperty('computeToast')
+        },
+        edgeAdded(edge) {
+            return edge
         }
         
     }
