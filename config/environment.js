6baf05f3cdd2c94085daaa84a014a67b8ae58505
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-21 22:37:59 -0400

    add gh-pages env

--- config/environment.js
+++ config/environment.js
@@ -31,6 +31,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
+  if (environment === 'gh-pages') {
+    ENV.rootURL = '/root-cause-ai/';
+  }
 
   if (environment === 'test') {
     // Testem prefers this...
