0faff7f3861c5292d12ca1faa18549f5cfc7a0a5
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2018-04-22 00:25:25 -0400

    Commit: - added comments - map for nodes (contant time lookup) - began forming the computedPaths functions

--- app/pods/components/root-cause-analysis/component.js
+++ app/pods/components/root-cause-analysis/component.js
@@ -1,12 +1,26 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
-import { A } from '@ember/array';
+//import { A } from '@ember/array';
 
 import moment from 'moment';
 
 export default Component.extend({
     startNode: 0,
     endNode: 1,
+
+    objectMap: computed( 'model', function() {
+        const edges = this.get('model')
+        let objectMap = new Map()
+        for(const edge of edges){
+            objectMap(edge.id, edge.data)
+        }
+        return objectMap
+    }),
+    /* 
+     * Description: 2D array of all paths that connect startNode and endNode
+     * Complexity: Exponential time complexity (doesnt scale)
+     * Purpose: This is meant to allow comparison from the heuristic based solution to caclulate successfull root cause analysis
+    */
 
     allPaths: computed( 'model', function() {
         const startTime = moment.now()
@@ -47,10 +61,23 @@ export default Component.extend({
         }
 
         this.setStats({'time': (moment.now() - startTime) /1000, 'iterations': iterations})
-        return paths.sort(function(a, b) {
-            return a.length - b.length || // sort by length, if equal then
-                   a.localeCompare(b);    // sort by dictionary order
-          });
+        return paths.sort(function(a, b) { return a.length - b.length || a.localeCompare(b)})
+    }),
+
+    /* 
+     * Description: Array of objects representing all paths from startNode to endNode
+     * Complexity: Linear time complexity
+     * Purpose: This will be triggered with updates to the allPaths, purpose is to generate goodness scores for paths 
+    */
+
+    computedPaths: computed( 'allPaths', function() {
+        const allPaths = this.get('allPaths')
+        let computedPaths = []
+
+        for(const path of allPaths){
+            let computedPath = {}
+        }
+        return computedPaths
     }),
 
     setStats (stats) {
